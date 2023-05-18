af32b89709b8f7684b9fc1dcc04905a43cbee508
beebrown <bruno.grosdesirs@outlook.fr>
2023-05-18 03:00:06 +0200

    maj api

--- scripts/api/api.js
+++ scripts/api/api.js
@@ -1,14 +1,12 @@
-let data = null;
+let data = false;
 //Récupération des infos du fichier photographer.json
 export async function api() {
   if (!data) {
-    console.log("fetch en cours...");
     data = await fetch("/data/photographers.json")
       .then(response => response.json())
       .catch(error => console.log(error));
     return data;
   } else {
-    console.log("récupération des données stockées !");
     return data;
   }
 };
